b051e825ac1f61bd836823ad9be2e8f61ad68031
Ana Elisa Estrada <anaelisaestrada@gmail.com>
2021-10-13 12:13:46 -0700

    Cronometro meditacion

--- AppNutricion/AppNutricion/Habitos/ViewControllerMeditacion.swift
+++ AppNutricion/AppNutricion/Habitos/ViewControllerMeditacion.swift
@@ -11,6 +11,11 @@ class ViewControllerMeditacion: UIViewController {
 
     @IBOutlet weak var btnGuardar: UIButton!
     @IBOutlet weak var btnIniciar: UIButton!
+    @IBOutlet weak var lbCronometro: UILabel!
+    
+    var timer:Timer = Timer()
+    var count:Int = 0
+    var timerCounting:Bool = false
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -21,7 +26,39 @@ class ViewControllerMeditacion: UIViewController {
         // Do any additional setup after loading the view.
     }
     
-
+    @IBAction func btnStart(_ sender: UIButton) {
+        if(timerCounting){
+            timerCounting = false
+            timer.invalidate()
+            btnIniciar.setTitle("Iniciar", for: .normal)
+        }else{
+            timerCounting = true
+            btnIniciar.setTitle("Detener", for: .normal)
+            timer = Timer.scheduledTimer(timeInterval: 1, target: self, selector: #selector(timerCounter), userInfo: nil, repeats: true)
+        }
+    }
+    
+    @objc func timerCounter() -> Void {
+        count = count + 1
+        let time = secondsToHoursMinutesSeconds(seconds: count)
+        let timeString = makeTimerString(hours: time.0, minutes: time.1, seconds: time.2)
+        lbCronometro.text = timeString
+    }
+    
+    func secondsToHoursMinutesSeconds(seconds:Int) -> (Int,Int,Int){
+        return (seconds / 3600, (seconds % 3600)/60, ((seconds % 3600)%60))
+    }
+    
+    func makeTimerString(hours: Int, minutes: Int, seconds: Int) -> String{
+        var timeString = ""
+        timeString += String(format: "%02d", hours)
+        timeString += " : "
+        timeString += String(format: "%02d", minutes)
+        timeString += " : "
+        timeString += String(format: "%02d", seconds)
+        return timeString
+    }
+    
     /*
     // MARK: - Navigation
 
