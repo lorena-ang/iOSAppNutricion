fc73c63fd40b496b2041ba5c7ee379b357683537
Ana Elisa Estrada <anaelisaestrada@gmail.com>
2021-10-13 01:39:57 -0700

    Pantalla pasos

---
+++ AppNutricion/AppNutricion/Habitos/ViewControllerPasos.swift
@@ -0,0 +1,31 @@
+//
+//  ViewControllerPasos.swift
+//  AppNutricion
+//
+//  Created by Chut on 13/10/21.
+//
+
+import UIKit
+
+class ViewControllerPasos: UIViewController {
+    
+    @IBOutlet weak var tfPasos: UITextField!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
